50b5bd69f28c8f3af4cd863f5fe632d00802644a
yuyanpsy <yuyanpsy@gmail.com>
2026-04-22 19:46:15 +0800

    启用edge-to-edge全屏，status bar透明，消除页面切换闪动

--- app/src/main/java/com/psymap/app/MainActivity.kt
+++ app/src/main/java/com/psymap/app/MainActivity.kt
@@ -2,13 +2,21 @@ package com.psymap.app
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                android.graphics.Color.TRANSPARENT,
+                android.graphics.Color.TRANSPARENT
+            )
+        )
         super.onCreate(savedInstanceState)
         setContent {
             PsyMapTheme {
